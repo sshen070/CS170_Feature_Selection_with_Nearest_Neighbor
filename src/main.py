312c467166e5ff9f39ee0c14fc74dd9d9dd7ae00
sshen070 <SujithShen@gmail.com>
2026-03-10 13:10:29 -0700

    Sucessfully loaded CSV data into an array format

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+# Convert CSV data into array format for easy access during Feature Selection
+def load_data(input_path):
+    # Stores every record of CSV
+    data = []
+
+    with open(input_path, "r") as file:
+        for line in file:
+            record = []
+            values = line.strip().split()
+
+            for i in range(len(values)):
+                record.append(values[i])
+
+            data.append(record)
+
+    return data
+
+def main():
+    data_set = input('Enter Data Set FILENAME: ')
+    input_path = Path('../data/') / data_set
+
+    print(f"Using file: {input_path}")
+
+    data_arr = load_data(input_path)
+    # print(data_arr)
+    
+
+if __name__ == "__main__":
+    main()
