e9f67efc150b55cc83932d44f63b8beceb95afb1
sshen070 <SujithShen@gmail.com>
2026-03-15 00:21:12 -0700

    Sucessfully implemented Backwards Elimination Search

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -13,7 +13,7 @@ def forward_selection(data_arr):
     best_overall_accuracy = 0 
     best_set = set()
 
-    print('Beginning Search\n')
+    print('Beginning Forward Selection\n')
     # N iterations (1 per level of parsing)
     for i in range(num_features):
 
@@ -54,10 +54,65 @@ def forward_selection(data_arr):
 
         print(f'Feature set {current_set} was best, accuracy is {best_accuracy_curr_level}\n')
 
-    print(f'Finished search! The best feature subset is {best_set}, which has an accuracy of {best_overall_accuracy}')
+    print(f'Finished Forward Selection! The best feature subset is {best_set}, which has an accuracy of {best_overall_accuracy}')
 
 
-# Unimplemented ~ Testing search
+def backward_elimination(data_arr):
+
+    # First column is the cluster record belongs to
+    num_features = len(data_arr[0]) - 1
+
+    # Start with full set & move backwards
+    current_set = set()
+    for i in range(1, num_features + 1):
+        current_set.add(i)
+
+    # Keep track of global variables for optimal subset & accuracy across all features
+    best_overall_accuracy = 0 
+    best_set = current_set.copy()
+
+    print('Beginning Backward Elimination\n')
+
+    # Continue to run until only one feature remains (no more feature elimination can be performed)
+    while(len(current_set) > 1):
+
+        feature_to_remove_at_this_level = None
+
+        # Keep track of the best potential features based current iteration (non-global)
+        best_accuracy_curr_level = 0
+
+        for k in current_set:
+            potential_set = current_set.copy()
+            potential_set.remove(k)
+            
+            accuracy = leave_one_out_cross_validation(
+                data_arr, potential_set, best_accuracy_curr_level
+            )
+
+            if (accuracy > best_accuracy_curr_level):
+                best_accuracy_curr_level = accuracy
+                feature_to_remove_at_this_level = k
+        
+            print(f'\tUsing features(s) {potential_set} accuracy is {accuracy}')
+        
+        current_set.remove(feature_to_remove_at_this_level)
+
+        if best_accuracy_curr_level < best_overall_accuracy:
+            print('\n(Warning, Accuracy has decreased! Continuing search in case of local maxima)')
+
+        else:
+            
+            # Keep track of optimal set of features & accuracy
+            best_set = current_set.copy()
+            best_overall_accuracy = best_accuracy_curr_level
+            print('\n')
+
+        print(f'Feature set {current_set} was best, accuracy is {best_accuracy_curr_level}\n')
+
+    print(f'Finished Backward Elimination! The best feature subset is {best_set}, which has an accuracy of {best_overall_accuracy}')
+
+
+
 def leave_one_out_cross_validation(data_arr, potential_set, best_accuracy_curr_level):
     
     number_correctly_classified = 0
